e05fcc37ab6cc7afdbd827630b8e5661b030953b
mindpaceltd <andeoyebrayan@gmail.com>
2026-04-30 11:15:03 +0300

    chore: remove temp files

--- write-blog-page.cjs
+++
@@ -1,6 +0,0 @@
-﻿const fs = require('fs');
-const path = require('path');
-const outPath = path.join(__dirname, 'src', 'app', '(public)', 'blog', '[slug]', 'page.tsx');
-const c = [];
-const L = (s) => c.push(s);
-
